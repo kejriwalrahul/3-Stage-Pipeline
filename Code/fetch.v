e1925de343020a7a73e8a20e8736ff083f9d2a56
Rahul Kejriwal <kejriwalrahul@outlook.com>
2017-02-10 12:51:53 +0530

    added fetch testbench

--- Code/fetch.v
+++ Code/fetch.v
@@ -9,35 +9,29 @@ module fetchInstruction(
 		input clk,
 		input rst,
 
-		output[7:0] instr_addr,
+		output[7:0] prog_ctr,
 		input[15:0] instr,
 
 		output[15:0] curr_instr
 	);
 
-	reg[ 7:0] instr_addr;
-
-	// Insturction Register
-	reg[15:0] curr_instr;
 	// Program Counter
 	reg[ 7:0] prog_ctr;
-
+	// Instruction Register
+	reg[15:0] curr_instr;
 
 	initial begin
- 		instr_addr 	= 8'b0;
  		prog_ctr	= 8'b0;
  		curr_instr	= 16'b0;		
 	end
 
  	always @(posedge rst) begin
- 		instr_addr 	= 8'b0;
  		prog_ctr	= 8'b0;
  		curr_instr	= 16'b0;
  	end
 
  	always @(posedge clk) begin
 	 	curr_instr = instr;
- 		instr_addr += 1;
+ 		prog_ctr += 1;
  	end
-
 endmodule
---
+++ Testbench/fetch_testbench.v
@@ -0,0 +1,44 @@
+/*
+	Module written by Rahul Kejriwal
+	CS14B023
+
+	Synchronous module to fetch instructions
+*/
+
+`include "../Code/fetch.v"
+
+module fetch_TestBench(
+		output clk,
+		output rst,
+
+		input[7:0] instr_addr,
+		output[15:0] instr,
+
+		input[15:0] curr_instr
+	);
+
+	reg clk, rst;
+	reg[15:0] instr;
+
+	fetchInstruction I(clk, rst, instr_addr, instr, curr_instr);
+
+	always begin
+		#10 clk = ~clk;
+	end
+
+	initial begin
+		$dumpfile("fetch.vcd");
+    	$dumpvars(0,fetch_TestBench);
+
+		clk = 0;
+		rst = 0;
+		instr = 0;
+	
+		$monitor($time,, "clk: %b, Address: %b, IR: %b, ISent: %b", clk, instr_addr, curr_instr, instr);
+		#50 $finish;
+	end
+
+	always @(instr_addr) begin
+		instr = instr_addr << 8 | instr_addr ^ 8'b11111111;
+	end 
+endmodule
